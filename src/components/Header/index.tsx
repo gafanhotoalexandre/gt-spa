04510d41add9ad348af4f3b09ff4d832953e37f0
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-07-10 10:59:05 -0300

    melhorando responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,20 +5,25 @@ import './Header.css'
 export function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
+      <div>
+        <h2 id='logo'>
+          <Link to='/'>LOGO</Link>
+        </h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
 
-          <li>
-            <Link to='/sobre'>Sobre</Link>
-          </li>
-          <li>
-            <Link to='/posts'>Postagens</Link>
-          </li>
-        </ul>
-      </nav>
+            <li>
+              <Link to='/sobre'>Sobre</Link>
+            </li>
+            <li>
+              <Link to='/posts'>Postagens</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
